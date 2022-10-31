b24c84f46106e3a950fd99ee7d95a03c09a92d51
李晏慈 <3a832005@gm.student.ncut.edu.tw>
2022-11-01 01:16:22 +0800

    設置Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\HTTP\Controllers\HomeController;
+use APP\HTTP\Controllers\AboutController;
+use APP\HTTP\Controllers\NewsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[\App\Http\Controllers\HomeController::class,'index'])->name('home.index');
+Route::get('about',[\App\Http\Controllers\AboutController::class,'index'])->name('about.index');
+Route::get('news',[\App\Http\Controllers\NewsController::class,'index'])->name('news.index');
+
